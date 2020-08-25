4fcc711ca6240bdb710369633f04ca6a657f9cd5
elvelive <elve2002@live.se>
2020-08-25 09:37:06 +0200

    server and user info commands added, default error handling

--- src/index.js
+++ src/index.js
@@ -1,23 +1,33 @@
 require('dotenv').config()
 
 const Discord = require('discord.js')
+
 const client = new Discord.Client()
+
 const PREFIX = "&";
 
+// Colors
+const fgYellow = "\x1b[33m%s\x1b[33m"
+const fgMagenta = "\x1b[35m%s\x1b[35m"
+const fgCyan = "\x1b[36m%s\x1b[36m"
+
+
+// Init
 client.once('ready', () => {
-  console.log("\x1b[35m%s\x1b[35m", `\nLogged in as ${client.user.tag}!`)
+  console.log(fgMagenta, `\nLogged in as ${client.user.tag}!`)
 })
+
 
 client.on('message', (message) => {
   if (message.author.bot) return;
   if (!message.author.bot && !message.content.startsWith(PREFIX)) {
       console.log(
-        "\x1b[36m%s\x1b[36m",
+        fgCyan,
         `\nUser [${message.author.tag}] sent message: ${message.content}`)
   } else if (!message.author.bot && message.content.startsWith(PREFIX)) {
     console.log(
-      "\x1b[33m%s\x1b[33m",
-      `\n${Date()} User [${message.author.tag}] called command: ${message.content.trim()}`
+      fgYellow,
+      `\nUser [${message.author.tag}] called command: ${message.content.trim()}`
     );
   }
 
@@ -29,11 +39,20 @@ client.on('message', (message) => {
 
 
     switch (CMD) {
-      case "kick":
-        message.channel.send("User kick")
+      case "server":
+        message.channel
+          .send(`Server name: ${message.guild.name}\nTotal members: ${message.guild.memberCount}\nCurrent owner: ${message.guild.owner}`)
+          .then(console.log(fgYellow, "Command &server completed successfully"))
+        break;
+      case "user-info":
+        message.channel
+          .send(`Your username: ${message.author.toString()}`)
+          .then(console.log(fgYellow, "Command &user-info completed successfully"))
+        break;
+      default:
+        message.channel.send("Not a valid command!")
     }
   }
-
 })
 
 client.login(process.env.BOT_TOKEN);
