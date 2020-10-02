b8adec07b9635194178b0ae8336ea175fc13ddcd
elvelive <elve2002@live.se>
2020-10-02 10:23:23 +0200

    reworked server command to json/js object embed

--- src/commands/server.js
+++ src/commands/server.js
@@ -6,27 +6,50 @@ module.exports = {
   description: "Displays info about the server",
   execute(m) {
     try {
-      const embed = new MessageEmbed()
-        .setColor('#0099ff')
+      const serverEmbed = {
+        color: '0099ff',
+        title: '**Server Info**',
+        description: this.description.toString(),
+        fields: [
+          {
+            name: '\u200b',
+            value: '\u200b',
+            inline: false,
+          },
+          {
+            name: 'Server name: ',
+            value: m.guild.name,
+            inline: true,
+          },
+          {
+            name: 'Member count: ',
+            value: m.guild.memberCount,
+            inline: true,
+          },
+          {
+            name: '\u200b',
+            value: '\u200b',
+            inline: false,
+          },
+          {
+            name: 'Current owner: ',
+            value: m.guild.owner,
+            inline: true,
+          },
+          {
+            name: 'Server created: ',
+            value: m.guild.createdAt.toLocaleDateString(),
+            inline: true,
+          },
+        ],
+        footer: {
+          name: 'Powered by Impulse',
+        }
+      }
 
-        .setTitle('**Server Info**')
-        .setDescription(this.description.toString())
-        .addField('\u200b', '\u200b')
-
-        .addFields(
-          { name: 'Server name: ', value: m.guild.name, inline: true },
-          { name: 'Member count: ', value: m.guild.memberCount, inline: true }
-        )
-        .addField('\u200b', '\u200b')
-        .addFields(
-          { name: 'Current owner: ', value: m.guild.owner, inline: true },
-          { name: 'Server created: ', value: m.guild.createdAt.toLocaleDateString(), inline: true }
-        )
-
-        .setFooter('Powered by Impulse')
-      m.channel.send(embed)
+      m.channel.send({ embed: serverEmbed })
     } catch (err) {
-      console.log(color.fgRed, `Error when calling command, ${err}`)
+      console.err(color.fgRed, `Error when calling command, ${err}`)
     }
     console.log(color.fgYellow, 'Command &server completed successfully\n')
   }
